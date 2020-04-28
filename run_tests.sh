bd62cbbb8d3b70ab5798612f804596bb08c347d0
Hung Luong Do Minh <minhhung2556@gmail.com>
2020-04-28 21:49:04 +0700

    update script to run integration test on android emulator, ios simulator, chrome

--- run_tests.sh
+++ run_tests.sh
@@ -27,6 +27,59 @@ else
   echo "no need to update"
 fi
 
-flutter test test/home_page_bloc_test.dart
-flutter test test/home_page_test.dart
-flutter drive --target=test_driver/app.dart --driver=test_driver/app_test.dart
+runUnitTests() {
+    flutter test test/home_page_bloc_test.dart
+}
+
+runWidgetTests() {
+	flutter test test/home_page_test.dart	
+}
+
+runIntegrationTests() {
+	devicesStr=$(flutter devices)
+	echo $devicesStr
+
+	devicesArray=(${devicesStr// • / })
+
+	devices=()
+	deviceCount=0
+	for (( i=0; i<${#devicesArray[@]}; i++ ))
+	do
+		item=${devicesArray[$i]}
+		# echo "item $i: $item"
+		if [ $i>0 ]; then
+			if [[ $item == android-* ]] || [[ $item == "ios" ]]; then
+				j=$((i-1))
+				# echo "j: $j"
+				devices[$deviceCount]=${devicesArray[j]}
+				echo "devices[$deviceCount]: ${devices[$deviceCount]}"
+				((deviceCount++))
+			fi
+			if [[ $item == "chrome" ]]; then
+				devices[$deviceCount]=$item
+				echo "devices[$deviceCount]: ${devices[$deviceCount]}"
+				((deviceCount++))
+			fi
+	   	fi
+	done
+
+	for (( i=0; i<$deviceCount; i++ ))
+	do
+		item=${devices[$i]}
+		echo "run test on $item"
+
+		if [[ $item == "chrome" ]]; then
+			~/chromedriver --port=4444
+			flutter drive --target=test_driver/app.dart --keep-app-running -d $item --browser-name=$item --profile
+		else
+			flutter drive --target=test_driver/app.dart --keep-app-running -d $item		
+		fi
+	done
+}
+
+runUnitTests
+runWidgetTests
+runIntegrationTests
+
+
+
